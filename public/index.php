753a848984f551afc964de9866ef45e536878de4
Albert Moreno <albert.moreno.dev@gmail.com>
2016-10-19 22:40:06 +0200

    Activate gentelella Bootstrap template

--- public/index.php
+++ public/index.php
@@ -7,27 +7,14 @@ include "../vendor/autoload.php";
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Server Admin</title>
 
-        <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        <link href="bower/gentelella/build/css/custom.min.css" rel="stylesheet">
     </head>
     <body>
         <?php $disk = new \ServerInfo\Disk(); ?>
         <h1><?=$disk->freeSpace();?> of <?=$disk->totalSpace();?></h1>
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
+        <script src="bower/gentelella/build/js/custom.min.js"></script>
     </body>
 </html>
